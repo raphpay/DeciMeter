3da7f9d9011ac953e60d0e3b64057d77a8a5df63
Raphaël Payet <raphpay.dev@gmail.com>
2023-07-23 14:50:44 +0200

    First commit

---
+++ DeciMeter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DeciMeter
+//
+//  Created by Raphaël Payet on 23/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DeciMeter/DeciMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeciMeterApp.swift
+//  DeciMeter
+//
+//  Created by Raphaël Payet on 23/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct DeciMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
